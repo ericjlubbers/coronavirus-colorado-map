41ff2d4a92376979be61991c123a4f4c94c04592
ericjlubbers <ericjlubbers@gmail.com>
2020-03-06 18:25:20 -0700

    initial commit

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1tkBfr8_k5FR01IAm6EQqZvtUu4P7mxuBpTHezcim2ng/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WljeDZbeg5BLTa-GGc1PkB5g_8Ah_vAmwpIeesnfIGU/edit#gid=0';
